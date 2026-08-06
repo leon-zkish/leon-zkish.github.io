cf599fbfe2f551c7c3dc003e946637abe0442112
ZKISH <61718116+leon-zkish@users.noreply.github.com>
2026-08-05 20:42:05 -0400

    Update connection.js

--- js/connection.js
+++ js/connection.js
@@ -2,7 +2,7 @@ class ZkishConnection extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <style>
-            .connection-box {position: fixed;top: 4px;left: 4px;background: #111;color: white;padding: 8px 16px;border: 1px solid #6b00a1;border-radius: 5px;font-family: Consolas, monospace;font-size: 12px;z-index: 9999;
+            .connection-box {position: fixed;top: 4px;left: 4px;background: #111;color: white;padding: 8px 16px;border: 1px solid #6b00a1;border-radius: 5px;font-family: Consolas, monospace;font-size: 16px;z-index: 9999;
             }
         </style>
         <div class="connection-box">
@@ -14,17 +14,17 @@ class ZkishConnection extends HTMLElement {
             try {
                 await fetch(window.location.href + "?ping=" + Date.now(), {cache: "no-store"});
                 const ms = Math.round(performance.now() - start);
-                let status;
+                let msg;
                 if (ms < 80)
-					{status = "ZKISH.net: CONNECTED";
+					{msg = "ZKISH.net: CONNECTED";
                 } else if (ms < 180)
-					{status = "ZKISH.net: LOW LATENCY";
+					{msg = "ZKISH.net: LOW LATENCY";
                 } else if (ms < 400)
-					{status = "ZKISH.net: HIGH LATENCY (SLOW)";
+					{msg = "ZKISH.net: HIGH LATENCY (SLOW)";
                 } else
-					{status = "ZKISH.net: UNSTABLE";}
+					{msg = "ZKISH.net: UNSTABLE";}
 
-                box.textContent = `Connection: ${ms}ms - ${status}`;
+                box.textContent = `${msg}.<br>Connection: ${ms}ms.<br>"User: Not logged in."`;
             } catch {box.textContent = "ZKISH.net: OFFLINE";}
         }
         checkConnection();
